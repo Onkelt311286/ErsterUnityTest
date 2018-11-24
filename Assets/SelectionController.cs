367129b73fdd8849ea543bbde035fc602c1e9b11
Onkelt311286 <onkelt311286@gmail.com>
2018-11-24 15:23:09 +0100

    MyCube YourCube HisCube
    MyCube YourCube HisCube

---
+++ Assets/SelectionController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionController : MonoBehaviour{
+
+    private void OnMouseUpAsButton()
+    {
+        Debug.Log("Selected: " + gameObject.name);
+    }
+}
